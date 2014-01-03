62f8257ae7903d7087951a63f0abe87ca1f59422
Pathé SENE <pathe.sene@gmail.com>
2014-01-03 00:14:49 +0000

    Ajout de la classe abstraite Spectacle Ajout de la classe Concert héritant de spectacle Ignore les répertoires bin et obj dans le projet spectacles

---
+++ Spectacles/Concert.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xarala.Mairie.Spectacles
+{
+    public class Concert : Spectacle
+    {
+        private string _stade;
+
+        private string _artiste;
+
+        private DateTime _heure;
+
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructeur à cinq arguments. 
+        /// Appelle le constructeur de la classe de base
+        /// <see cref="Spectacle"/>
+        /// </summary>
+        /// <param name="stade">Le lieu du spectacle.</param>
+        /// <param name="artiste">Le nom de l'artiste ou de l'orchestre qui anime le spectacle.</param>
+        /// <param name="date">La date et l'heure du spectacle.</param>
+        /// <param name="nom">Le nom du spectacle.</param>
+        /// <param name="publicCible">Le public cible su spectacle.</param>
+        public Concert( string stade, string artiste, DateTime date, string nom, PublicCible publicCible) : base (nom, date, publicCible)
+        {
+            _stade = stade;
+            _artiste = artiste;
+            _heure = date;
+        }
+
+        #endregion
+
+
+        #region Method Overrides
+
+        /// <summary>
+        /// Surcharge de la méthode ToString()
+        /// </summary>
+        /// <returns>string Le concert au format chaine de caractères.</returns>
+        public override string ToString()
+        {
+            StringBuilder concert = new StringBuilder();
+
+            concert.Append(Nom.ToUpper());
+            concert.Append(String.Format("\n\tPublic Cible: {0}", PublicCibleSpectacle.ToString()));
+            concert.Append("\n\tType: CONCERT");
+            concert.Append(String.Format("\n\tArtiste: {0}", _artiste));
+            concert.Append(String.Format("\n\tHeure: {0}:{1}", _heure.Hour, _heure.Minute));
+            concert.Append(string.Format("\n\tLieu: STADE {0}", _stade.ToUpper()));
+
+            return concert.ToString();
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ Spectacles/Spectacle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xarala.Mairie.Spectacles
+{
+    /// <summary>
+    /// Abstraction d'un spectacle
+    /// </summary>
+    public abstract class Spectacle
+    {
+
+        private string _nom;
+
+        private DateTime _date;
+
+        private PublicCible _publicCible; // force typo! preventing clash
+
+        /// <summary>
+        /// Constructeur de spectacle avec le nom, la date et le public cible.
+        /// </summary>
+        /// <param name="nom">Le nom du spectacle.</param>
+        /// <param name="date">La date du spectacle (avec l'heure de début).</param>
+        /// <param name="publicCible">Le public cible. <see cref="PublicCible"/></param>
+        public Spectacle(string nom, DateTime date, PublicCible publicCible)
+        {
+            _nom      = nom;
+            _date     = date;
+            _publicCible = publicCible;
+        }
+
+        #region Propriétés
+
+        public string Nom { 
+            get { return _nom; } 
+        }
+
+        public PublicCible PublicCibleSpectacle
+        {
+            get { return _publicCible; }
+        }
+
+        #endregion
+
+    }
+
+    public enum PublicCible // force typo! preventing clash
+    {
+        Jeune = 1, 
+        Adulte, 
+        Tous// jeune et adulte [TODO refactoring]        
+    }
+
+}
